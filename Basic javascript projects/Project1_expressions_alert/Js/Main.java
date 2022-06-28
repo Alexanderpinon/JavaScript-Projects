ea83ca55a96fc5c6868a687bcefe6d19b8dc8428
Alexanderpinon <anderspin99@icloud.com>
2022-06-27 22:49:52 -0700

    made java file

---
+++ Basic javascript projects/Project1_expressions_alert/Js/Main.java
@@ -0,0 +1,12 @@
+var Family = "Pinons", Dad = "Alexander", Mom = "Maya"
+Daughter = "Snowy", Son = "Boomer"; // this is my multiple variables
+document.write(Mom); //calling variable for mom
+document.write(3+3); //adding 3+3
+
+window.alert("hi") // this displays hi in thw window pop up
+var A = "This is the beginning of the string" // i am saying this is variable A
+var B = "this is the end of the string" // i am saying this is variable b
+document.write(A) // calling out variable A
+document.write(B) // calling variable B
+document.write("\"This is the beginning of the string,"
++ "this is the end of the string"); // i am concatenate the string
