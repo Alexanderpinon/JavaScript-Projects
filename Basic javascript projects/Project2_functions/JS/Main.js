d1e6966bbc16c6244837fddc2dbb391cd6de5895
Alexanderpinon <anderspin99@icloud.com>
2022-06-28 20:34:09 -0700

    added java script code

---
+++ Basic javascript projects/Project2_functions/JS/Main.js
@@ -0,0 +1,5 @@
+function MyFunction() { // i am adding a function
+    var sentence = "I am learning"; // adding a sentence
+    sentence += "alot from this book"; // i am concatenate a string
+    document.getElementById("Concatenate").innerHTML = sentence; 
+}
